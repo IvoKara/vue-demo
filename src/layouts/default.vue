a0359345fc275614e0b945475afb32f8e34b50d4
Ivo <ivo.i.karaneshev@gmail.com>
2022-07-13 18:47:43 +0300

    add state managment: pinia

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import { Head } from '@vueuse/head'
 import { useDark } from '@vueuse/core'
+import { storeToRefs } from 'pinia'
 import { isDark } from '@/composables/dark'
+import { useUserStore } from '@/stores/user'
+const auth = useUserStore()
+const { isLoggedin } = storeToRefs(auth)
+onMounted(() => {
+  setTimeout(() => isLoggedin.value = true, 2999)
+})
 </script>
 
 <template>
   <main my-10 px-4 py-5 text-center>
+    {{ isLoggedin }}
     <RouterView />
   </main>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import PerfectScrollbar from 'vue3-perfect-scrollbar'
 import { Head, createHead } from '@vueuse/head'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import '@/../node_modules/nprogress/nprogress.css'
@@ -10,12 +12,11 @@ import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css'
 import '@/../node_modules/daisyui/dist/full.css'
 import '@/../node_modules/daisyui/dist/themes.css'
 
-const app = createApp(App)
+const pinia = createPinia()
 const head = createHead()
-
-app.use(head)
-app.use(PerfectScrollbar)
-app.use(router)
-app.component('Head', Head)
-app.mount('#app')
+createApp(App).use(head)
+  .use(pinia)
+  .use(router)
+  .component('Head', Head)
+  .mount('#app')
 
