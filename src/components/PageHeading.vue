4be3acd5f73c0ab1133f2d0589b4cf582cf835d3
Ivo <ivo.i.karaneshev@gmail.com>
2022-07-22 17:21:23 +0300

    Layout Shift About page heading fixing test

--- src/components/PageHeading.vue
+++ src/components/PageHeading.vue
@@ -4,8 +4,8 @@ defineProps<{
 }>()
 </script>
 
-<template lang="">
-  <h1 text-center mx-auto text="38" font-greatvibes>
+<template>
+  <h1 text-center mx-auto text="38" font-greatvibes h-57 w-full>
     {{ value }}
   </h1>
 </template>
